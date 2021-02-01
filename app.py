86b24bdaff15af4e249c5dd1fbda4d4a556cbce3
julianjgabler <julian.gabler@rwth-aachen.de>
2021-02-01 11:35:13 +0100

    example

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ external_scripts = [
     'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js',
     'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js',
 ]
-
+print('Hello World')
 # Server definition
 
 server = flask.Flask(__name__)
